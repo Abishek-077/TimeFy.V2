3d1b4b3bef224ebdbc1fdc46d2df17279eab9d37
Abishek-077 <abishekxofficial987@gmail.com>
2025-02-24 11:22:51 +0545

    crated a migration file for user

--- server/app.js
+++ server/app.js
@@ -1,13 +1,29 @@
+require('dotenv').config({ path: `${process.cwd()}/.env` });
 const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const taskRoutes = require('./routes/taskRoutes');
+
+const authRouter = require('./route/authRoute');
+const catchAsync = require('./utils/catchAsync');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controller/errorController');
 
 const app = express();
-app.use(cors());
-app.use(bodyParser.json());
 
-app.use('/api/tasks', taskRoutes);
+app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// all routes will be here
+app.use('/api/v1/auth', authRouter);
+
+app.use(
+    '*',
+    catchAsync(async (req, res, next) => {
+        throw new AppError(`Can't find ${req.originalUrl} on this server`, 404);
+    })
+);
+
+app.use(globalErrorHandler);
+
+const PORT = process.env.APP_PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log('Server up and running', PORT);
+});
--- server/models/task.js
+++ server/models/task.js
@@ -1,26 +1,26 @@
-module.exports = (sequelize, DataTypes) => {
-  const Task = sequelize.define('Task', {
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    status: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false
-    },
-    est: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    },
-    act: {
-      type: DataTypes.INTEGER,
-      defaultValue: 0
-    },
-    note: {
-      type: DataTypes.TEXT,
-      allowNull: true
-    }
-  });
+// module.exports = (sequelize, DataTypes) => {
+//   const Task = sequelize.define('Task', {
+//     title: {
+//       type: DataTypes.STRING,
+//       allowNull: false
+//     },
+//     status: {
+//       type: DataTypes.BOOLEAN,
+//       defaultValue: false
+//     },
+//     est: {
+//       type: DataTypes.INTEGER,
+//       allowNull: false
+//     },
+//     act: {
+//       type: DataTypes.INTEGER,
+//       defaultValue: 0
+//     },
+//     note: {
+//       type: DataTypes.TEXT,
+//       allowNull: true
+//     }
+//   });
 
-  return Task;
-};
+//   return Task;
+// };
